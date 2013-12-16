1dccf5b48e8b76d57b18983c1bfd9493063688f7
TBSHRichard <tbsh_richard@blacksheepherd.net>
2013-12-15 21:54:04 -0500

    Added Tutorial 1 story overlay.

--- src/GameView.js
+++ src/GameView.js
@@ -8,6 +8,7 @@
 	p.assetQueue;
 	p.levelIndex;
 	p.levelClassesArray;
+	p.hasSelectedPower;
 	p.background;
 	p.level;
 	p.overlay;
@@ -22,6 +23,7 @@
 		this.assetQueue = assetQueue;
 		this.levelIndex = levelIndex;
 		this.levelClassesArray = levelClassesArray;
+		this.hasSelectedPower = false;
 		this.assetQueue = assetQueue;
 		this.width = width;
 		this.height = height;
@@ -40,36 +42,46 @@
 		this.addChild(this.background);
 		this.addChild(this.level);
 		this.addChild(this.overlay);
-		//this.addChild(new BeginningOverlay(66, width, height - 66, this.level.getBeginningOverlay(assetQueue)));
+		this.addChild(new BeginningOverlay(66, width, height - 66, this.level.getBeginningOverlay(assetQueue)));
 	}
 	
 	p.rotateCCW = function() { this.level.rotateCCW(); }
 	p.rotateCW = function() { this.level.rotateCW(); }
 	p.fireArrow = function() { this.level.fireArrow(); }
+	
 	p.switchPower = function(id) {
-		this.level.switchPower(id);
-		
-		switch(id) {
-			case 0:
-				this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-move-left"));
-				break;
-			case 1:
-				this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-move-right"));
-				break;
-			case 2:
-				this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-jump"));
-				break;
-			case 3:
-				this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-power-shot"));
-				break;
-			case 4:
-				this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-quiver"));
-				this.overlay.setBottomEnabled(0, false);
-				break;
+		console.log("out");
+		if (!this.hasSelectedPower) {
+			console.log("in");
+			this.hasSelectedPower = true;
+			this.removeChildAt(3);
+			
+			this.level.switchPower(id);
+			
+			switch(id) {
+				case 0:
+					this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-move-left"));
+					break;
+				case 1:
+					this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-move-right"));
+					break;
+				case 2:
+					this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-jump"));
+					break;
+				case 3:
+					this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-power-shot"));
+					break;
+				case 4:
+					this.overlay.setUsePowerButtonIcon(this.assetQueue.getResult("icon-quiver"));
+					this.overlay.setBottomEnabled(0, false);
+					break;
+			}
 		}
 	}
+	
 	p.usePower = function() { this.level.usePower(); }
 	p.stopPower = function() { this.level.stopPower(); }
+	p.hasSelectedPower = function() { return this.hasSelectedPower; }
 	
 	window.GameView = GameView;
 }())
